e5e9b8105b886280d8e2c8d3842f0dbfb2d162c9
Andy <ajemedafe@gmail.com>
2023-11-02 13:31:30 +0000

    adjust api functionality

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+// example location
+const location = "Tokyo";
 const axios = require("axios");
 const fs = require("fs");
 
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=ed26d6a8ccfdd6d65ff8cece7c01c685`;
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ed26d6a8ccfdd6d65ff8cece7c01c685`;
 
 router.get("/weather", (req, res) => {
 	const getLocation = async () => {
@@ -19,25 +20,12 @@ router.get("/weather", (req, res) => {
 	getLocation();
 });
 
-router.get("/weather/:id", (req, res) => {
-	const getLocation = async () => {
-		try {
-			const response = await axios.get(apiURL);
-
-			res.status(201).send(response.data.weather);
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
-	getLocation();
-});
-
 router.get("/weather/icon", (req, res) => {
 	const getImage = async () => {
 		try {
 			const response = await axios.get(apiURL);
 			const iconNo = response.data.weather[0].icon;
+			console.log(iconNo);
 			const iconUrl = `https://openweathermap.org/img/wn/${iconNo}.png`;
 			res.status(201).send(iconUrl);
 		} catch (error) {
@@ -48,29 +36,29 @@ router.get("/weather/icon", (req, res) => {
 	getImage();
 });
 
-router.get("/weather/name", (req, res) => {
-	const getName = async () => {
+router.get("/weather/location", (req, res) => {
+	const getLocation = async () => {
 		try {
 			const response = await axios.get(apiURL);
-			res.status(201).send(response.data.name);
+			res.status(200).send(response.data.name);
 		} catch (error) {
 			console.error(error);
 		}
 	};
-	getName();
+	getLocation();
 });
 
-router.get("/details", (req, res) => {
-	const getName = async () => {
+router.get("weather/details", (req, res) => {
+	const getDetails = async () => {
 		try {
 			const response = await axios.get(apiURL);
-			res.status(201).send(response.data);
+			res.status(201).send(response);
 		} catch (error) {
 			console.error(error);
 		}
 	};
 
-	getName();
+	getDetails();
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,5 +10,5 @@ app.use(cors());
 
 app.use(weatherRoutes);
 app.listen(SERVER_PORT, () => {
-	console.log(`this server is running on port ${SERVER_PORT}`);
+	console.log(`this server is running on port really ${SERVER_PORT}`);
 });
