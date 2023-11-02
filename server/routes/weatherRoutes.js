a559ed4a8a96e0f103bdd0ff074ed28f4fb4a9c6
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-11-02 16:41:12 +0000

    function done

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -4,50 +4,81 @@ const router = express.Router();
 const location = "Tokyo";
 const axios = require("axios");
 const fs = require("fs");
-
 // const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ed26d6a8ccfdd6d65ff8cece7c01c685`;
-
-function randomCities() {
-  const cities = [
-    "New York",
-    "Los Angeles",
-    "Chicago",
-    "London",
-    "Paris",
-    "Berlin",
-    "Tokyo",
-    "Sydney",
-    "Toronto",
-    "Miami",
-  ];
-
-  const randomIndex = Math.floor(Math.random() * cities.length);
-  return cities[randomIndex];
-}
-
-const randomLocation = randomCities();
-
-const randomURL = `https://api.tomorrow.io/v4/weather/forecast?location=${randomLocation}&apikey=ge7vkxnYkekeaFtbVijkKttMRsfTt6NS`;
-
+// const randomURL = `https://api.openweathermap.org/data/2.5/weather?q=${randomLocation}&appid=ed26d6a8ccfdd6d65ff8cece7c01c685`;
 router.get("/random", (req, res) => {
-  const getRandom = async () => {
-    try {
-      const response = await axios.get(randomURL);
-      console.log(response.data);
-      res.status(201).send(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  getRandom();
+  const file = fs.readFileSync("./data/data.json");
+  const data = JSON.parse(file);
+  function randomCities() {
+    const cities = fs.readFileSync("./data/data.json");
+    const data = JSON.parse(cities);
+    const randomIndex = Math.floor(Math.random() * data.length);
+    res.status(200).send(data[randomIndex]);
+  }
+  randomCities();
+});
+router.get("/weather", (req, res) => {
+  const file = fs.readFileSync("./data/data.json");
+  const data = JSON.parse(file);
+  console.log(data);
+  res.status(200).send(data);
 });
 
-// router.get("/random/icon", (req, res) => {
+router.get("/weather/:id", (req, res) => {
+  const file = fs.readFileSync("./data/data.json");
+  const data = JSON.parse(file);
+  console.log(data);
+  const singleWeather = data.find((weather) => weather.name === req.params.id);
+  console.log(singleWeather);
+  console.log(req.params);
+  // falsey used for situations for when the id doesn't exist
+  if (!singleWeather) {
+    res.status(404).send("note not found");
+    return;
+  }
+  console.log(data);
+  res.status(200).send(singleWeather);
+});
+
+module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+// // example location
+// const location = "Tokyo";
+// const axios = require("axios");
+// const fs = require("fs");
+
+// // const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ed26d6a8ccfdd6d65ff8cece7c01c685`;
+
+// function randomCities() {
+//   const cities = [
+//     "New York",
+//     "Los Angeles",
+//     "Chicago",
+//     "London",
+//     "Paris",
+//     "Berlin",
+//     "Tokyo",
+//     "Sydney",
+//     "Toronto",
+//     "Miami",
+//   ];
+
+//   const randomIndex = Math.floor(Math.random() * cities.length);
+//   return cities[randomIndex];
+// }
+
+// const randomLocation = randomCities();
+
+// const randomURL = `https://api.tomorrow.io/v4/weather/forecast?location=${randomLocation}&apikey=ge7vkxnYkekeaFtbVijkKttMRsfTt6NS`;
+
+// router.get("/random", (req, res) => {
 //   const getRandom = async () => {
 //     try {
 //       const response = await axios.get(randomURL);
-//       res.status(201).send(response.data.weather[0].icon);
+//       console.log(response.data);
+//       res.status(201).send(response.data);
 //     } catch (error) {
 //       console.error(error);
 //     }
@@ -56,83 +87,96 @@ router.get("/random", (req, res) => {
 //   getRandom();
 // });
 
-router.get("/random/location", (req, res) => {
-  const getLocation = async () => {
-    try {
-      const response = await axios.get(randomURL);
-      res.status(200).send(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  getLocation();
-});
+// // router.get("/random/icon", (req, res) => {
+// //   const getRandom = async () => {
+// //     try {
+// //       const response = await axios.get(randomURL);
+// //       res.status(201).send(response.data.weather[0].icon);
+// //     } catch (error) {
+// //       console.error(error);
+// //     }
+// //   };
 
-router.get("/random/details", (req, res) => {
-  const getDetails = async () => {
-    try {
-      const response = await axios.get(randomURL);
-      res.status(201).send(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+// //   getRandom();
+// // });
 
-  getDetails();
-});
+// router.get("/random/location", (req, res) => {
+//   const getLocation = async () => {
+//     try {
+//       const response = await axios.get(randomURL);
+//       res.status(200).send(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   getLocation();
+// });
 
-router.get("/weather", (req, res) => {
-  const getLocation = async () => {
-    try {
-      const response = await axios.get(apiURL);
-      res.status(201).send(response.data.weather);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+// router.get("/random/details", (req, res) => {
+//   const getDetails = async () => {
+//     try {
+//       const response = await axios.get(randomURL);
+//       res.status(201).send(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  getLocation();
-});
+//   getDetails();
+// });
 
-router.get("/weather/icon", (req, res) => {
-  const getImage = async () => {
-    try {
-      const response = await axios.get(apiURL);
-      const iconNo = response.data.weather[0].icon;
-      console.log(iconNo);
-      const iconUrl = `https://openweathermap.org/img/wn/${iconNo}.png`;
-      res.status(201).send(iconUrl);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+// router.get("/weather", (req, res) => {
+//   const getLocation = async () => {
+//     try {
+//       const response = await axios.get(apiURL);
+//       res.status(201).send(response.data.weather);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  getImage();
-});
+//   getLocation();
+// });
 
-router.get("/weather/location", (req, res) => {
-  const getLocation = async () => {
-    try {
-      const response = await axios.get(apiURL);
-      res.status(200).send(response.data.name);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  getLocation();
-});
+// router.get("/weather/icon", (req, res) => {
+//   const getImage = async () => {
+//     try {
+//       const response = await axios.get(apiURL);
+//       const iconNo = response.data.weather[0].icon;
+//       console.log(iconNo);
+//       const iconUrl = `https://openweathermap.org/img/wn/${iconNo}.png`;
+//       res.status(201).send(iconUrl);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-router.get("/weather/details", (req, res) => {
-  const getDetails = async () => {
-    try {
-      const response = await axios.get(apiURL);
-      res.status(201).send(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//   getImage();
+// });
 
-  getDetails();
-});
+// router.get("/weather/location", (req, res) => {
+//   const getLocation = async () => {
+//     try {
+//       const response = await axios.get(apiURL);
+//       res.status(200).send(response.data.name);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   getLocation();
+// });
 
-module.exports = router;
+// router.get("/weather/details", (req, res) => {
+//   const getDetails = async () => {
+//     try {
+//       const response = await axios.get(apiURL);
+//       res.status(201).send(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   getDetails();
+// });
+
+// module.exports = router;
